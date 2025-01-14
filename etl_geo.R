5a4d3af8fa5a9332bc500915353ec06bf6ce6f4d
Victoria Raucci <vickraucci@gmail.com>
2025-01-14 11:41:14 -0300

    criando a tabela 1

--- etl_geo.R
+++ etl_geo.R
@@ -2,15 +2,13 @@
 library(tidyverse)
 library(readr)
 library(tidyr)
-<<<<<<< HEAD
 library(ggplot2)
-
-=======
 library(dplyr)
->>>>>>> main
+
 # monitoring
-df_monit = read_delim("data/dados_monitoramento_cs_2024-03-22.csv",
-                      col_types = list(localidade = col_character(),
+
+df_monit <- read_delim("data/dados_monitoramento_cs_2024-03-22.csv",
+                       col_types = list(localidade = col_character(),
                                        data = col_date(format = "%d/%m/%Y"),
                                        visib_horiz = col_double(),
                                        faixa_bat = col_character(),
@@ -34,7 +32,19 @@ df_monit = read_delim("data/dados_monitoramento_cs_2024-03-22.csv",
 spec(df_monit)
 df_monit[2000,]
 
-# geomophology
+
+dfmonit_filt <- df_monit[ ,c("localidade", "n_trans_vis", "n_trans_pres")] %>%
+
+  
+  group_by(localidade) %>%
+  summarise()
+length(unique(dfmonit_filt$localidade))
+####tentando agrupar por localidade
+#####################
+         
+dfmonit_filt
+
+# geomorphology
 
 df_geo = read_delim("data/dados_geo_cs_2024-03-22.csv", 
                     col_types = list(localidade = col_character(),
@@ -52,7 +62,6 @@ df_geo = read_delim("data/dados_geo_cs_2024-03-22.csv",
                                      geo_id = col_double())
 )
 spec(df_geo)
-problems(df_geo)
 df_geo = df_geo[, 1:14]
 df_geo
 
@@ -63,30 +72,23 @@ df_localidade = read_delim("data/localidade_rebio.csv", delim = ";",
                            col_types = c("d","c","d"))
 df_localidade
 print(df_localidade, n = 41)
+#### o comprimento é baseado no mapa da localidade?
 
-
-
-####
-####
-# Tasks
 # Create a table id_dafor with the corresponding values iar_geo
 
-
-
-### test table dafor
+## test table dafor
 
 df_monit_test = df_monit %>% 
   filter(data > "2023-01-01",
          obs != "estimado dos dados do ICMBio",
          dafor > 0,
          faixa_bat != "entremare"
-<<<<<<< HEAD
-=======
          ) %>% 
   mutate(dafor_id = as.integer(dafor_id),
          geo_id = as.integer(geo_id))
 
-df_monit_test #ok
+df_monit_test 
+####vamos manter os filtros aplicados?
 
 ############################### resumo vic
 
@@ -95,19 +97,14 @@ df_monit_test_all = df_monit %>%
          obs != "estimado dos dados do ICMBio",
          #dafor > 0,
          faixa_bat != "entremare"
->>>>>>> main
-  ) %>% 
+         ) %>% 
   mutate(dafor_id = as.integer(dafor_id),
          geo_id = as.integer(geo_id))
 
-<<<<<<< HEAD
-df_monit_test[ , 12:15]  #ok
-
-=======
-df_monit_test_all#ok
+df_monit_test_all[, 1:17]
 
 length(unique(df_monit_test_all$dafor_id))
-# 69 transect
+####numero de valores diferentes na coluna do dafor id no df monit all
 
 df_monit_test_all %>%
   group_by(dafor_id) %>% 
@@ -121,7 +118,6 @@ df_monit_test_all_sum= df_monit_test_all %>%
   summarise(total_trans = max(n_trans_vis))
 
 sum(df_monit_test_all_sum$total_trans)
-# 3221
 
 3222/60 #HORas
 
@@ -144,21 +140,14 @@ print(df_monit_local, n=200)
  
 
 ############################ resumo vic
->>>>>>> main
 
 df_geo_test =    df_geo %>%
   filter(data > "2023-01-01",
          faixa_bat != "entremare"
-<<<<<<< HEAD
-  ) %>% 
-  mutate(geo_id = as.integer(geo_id))
-
-=======
          ) %>% 
   mutate(geo_id = as.integer(geo_id))
   
->>>>>>> main
-df_geo_test  #ok
+df_geo_test
 
 #leftjoin
 
@@ -175,17 +164,10 @@ df_monit_geo = df_monit_test %>%
          tempo_geo, 
          geo_cat, 
          iar_geo,
-<<<<<<< HEAD
   )
-=======
-        )
->>>>>>> main
 
 
 print(df_monit_geo, n = 50)
-
-
-
 
 # creating time in minutes to geo and investigating differences in time 
 
@@ -196,12 +178,7 @@ df_monit_geo = df_monit_geo %>%
   group_by(geo_id) %>% 
   mutate(tempo_geo_max = (max(tempo_geo)*5), 
          dif_t_dafor_geo = max(tempo_censo) - tempo_geo_max,
-<<<<<<< HEAD
   )
-=======
-         )
-
->>>>>>> main
 
 
 df_monit_geo
@@ -209,7 +186,6 @@ print(df_monit_geo, n = 500)
 
 
 # creating table
-
 
 df_model <- df_monit_geo %>%
   group_by(dafor_id) %>%
@@ -225,11 +201,6 @@ df_model <- df_monit_geo %>%
 df_model
 print(df_model, n = 148)  
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 #### AED
 
 df_aed <- df_monit_geo %>%
@@ -248,7 +219,6 @@ df_aed <- df_monit_geo %>%
     lg_sd = sd(iar_geo[geo_cat == "lg"], na.rm = TRUE)
   )
 
-<<<<<<< HEAD
 mean(df_aed$tf_avg)
 sd(df_aed$tf_sd)
 mean(df_aed$mp_avg)
@@ -276,8 +246,7 @@ shapiro.test(df_aed$rpm_avg)
 shapiro.test(df_aed$rpm_sd)
 shapiro.test(df_aed$lg_avg)
 shapiro.test(df_aed$lg_sd)
-=======
-df_aed
+
 
 #### com localidade
 
@@ -320,37 +289,13 @@ df_aed <- df_monit_geo %>%
 print(df_aed, n =12)
 
 
-
-
-# A tibble: 11 × 12
-#dafor_id n_trans tf_avg tf_sd mp_avg mp_sd gc_avg gc_sd rpm_avg rpm_sd lg_avg lg_sd
-#<int>   <dbl>  <dbl> <dbl>  <dbl> <dbl>  <dbl> <dbl>   <dbl>  <dbl>  <dbl> <dbl>
-#1       15       1   6    3.02    0.5  0.926  0     0       10     0       0.5  0.926
-#2       21       2   6    0       0.4  0.843  0     0        8.4   0.843   0    0    
-#3       22       4   6.75 1.41    4    2.03   1.75  1.59     6.75  2.68    0    0    
-#4       44       5   3.75 1.58    4.5  2.82   0     0        0.5   0.877   0.5  0.877
-#5       45       2   1.5  1.37    2.5  2.25   0     0        8.88  2.16    1.5  2    
-#6       58       1   6    0.894   7.27 2.24   5.45  2.02     3.82  1.40    3.45 1.29 
-#7       74       1   4.25 1.28    1.75 1.28   0.25  0.707    7.5   2.78    3.25 4.27 
-#8       99       2   8.44 1.89    6.44 3.60   4.67  2.57     6.44  2.33    0    0    
-#9      100      11   5.33 3.00    4.22 3.34   1.11  1.38     6.89  3.68    4.44 2.64 
-#10      118       3   6.33 3.16    3.08 2.83   0.833 1.01     9.25  1.42    2.25 3.03 
-#11      119       1   4    2.45    3.78 1.86   0     0        8.67  2.24    2.89 3.18 
-
-
-
->>>>>>> main
-
-
-
 mean(df_aed$n_trans)
-<<<<<<< HEAD
 sd(df_aed$n_trans)
 max(df_aed$n_trans)
 
 ## Analise de correlação
-cor(df_aed$n_trans, df_aed$tf_avg, method = "spearman") #sem correlacao
-cor(df_aed$n_trans, df_aed$mp_avg, method = "spearman") #sem correlacao
+cor(df_aed$n_trans, df_aed$tf_avg, method = "spearman") 
+cor(df_aed$n_trans, df_aed$mp_avg, method = "spearman") 
 cor(df_aed$n_trans, df_aed$gc_avg, method = "spearman") 
 cor(df_aed$n_trans, df_aed$rpm_avg, method = "spearman") 
 cor(df_aed$n_trans, df_aed$lg_avg, method = "spearman") 
@@ -404,62 +349,20 @@ cor(df_aed$n_trans, df_aed$gc_avg, method = "spearman")
 cor(df_aed$n_trans, df_aed$rpm_avg, method = "spearman")
 cor(df_aed$n_trans, df_aed$lg_avg, method = "spearman")
 
-#> cor(df_aed$n_trans, df_aed$tf_avg, method = "spearman")
-#[1] 0.07111002
-#> cor(df_aed$n_trans, df_aed$mp_avg, method = "spearman")
-#[1] 0.2818424
-#> cor(df_aed$n_trans, df_aed$gc_avg, method = "spearman")
-#[1] 0.1231662
-#> cor(df_aed$n_trans, df_aed$rpm_avg, method = "spearman")
-#[1] -0.314724
-#> cor(df_aed$n_trans, df_aed$lg_avg, method = "spearman")
-#[1] -0.163933
-
-
-
-
-
-
 mean(df_aed$tf_avg)
 sd(df_aed$tf_sd)
-#> mean(df_aed$tf_avg)
-#[1] 5.305556
-#> sd(df_aed$tf_sd)
-#[1] 0.9954703
 
 mean(df_aed$mp_avg)
 sd(df_aed$mp_sd)
-#> mean(df_aed$mp_avg)
-#[1] 3.4955
-#> sd(df_aed$mp_sd)
-#[1] 0.9220174
-
 
 mean(df_aed$gc_avg)
 sd(df_aed$gc_sd)
-#> mean(df_aed$gc_avg)
-#[1] 1.278696
-#> sd(df_aed$gc_sd)
-#[1] 0.9374563
-#> 
-
 
 mean(df_aed$rpm_avg)
 sd(df_aed$rpm_sd)
-#> mean(df_aed$rpm_avg)
-#[1] 7.008471
-#> sd(df_aed$rpm_sd)
-#[1] 1.055789
-
-
 
 mean(df_aed$lg_avg)
 sd(df_aed$lg_sd)
-#> mean(df_aed$lg_avg)
-#[1] 1.707989
-#> sd(df_aed$lg_sd)
-#[1] 1.469996
- 
 
 
 #######
@@ -578,4 +481,3 @@ ggplot(dt_melt, aes(x = variable, y = category, fill = value)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for readability
 
->>>>>>> main
