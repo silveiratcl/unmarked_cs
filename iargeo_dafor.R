d5fb4e18c3cdfc3c2e48d8ced87c61d160bdab6d
Victoria Raucci <vickraucci@gmail.com>
2025-01-22 15:08:31 -0300

    spread geo e padronização

---
+++ iargeo_dafor.R
@@ -0,0 +1,134 @@
+
+library(tidyverse)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(dplyr)
+
+
+# monitoramento 
+
+df_monit <- read_delim("data/dados_monitoramento_cs_2024-03-22.csv",
+                       col_types = list(localidade = col_character(),
+                                        data = col_date(format = "%d/%m/%Y"),
+                                        visib_horiz = col_double(),
+                                        faixa_bat = col_character(),
+                                        prof_interface_min = col_double(),
+                                        prof_interface_max = col_double(),
+                                        metodo = col_character(),
+                                        observer = col_character(),
+                                        n_divers = col_double(),
+                                        tempo_censo = col_double(),
+                                        dafor = col_double(),
+                                        iar_medio = col_double(),
+                                        n_trans_vis = col_double(),
+                                        n_trans_pres = col_double(),
+                                        dafor_id = col_double(),
+                                        geo_id = col_character(),
+                                        obs = col_character()
+                       ))
+
+
+
+spec(df_monit)
+df_monit
+print(df_monit, n = 100)
+
+## aplicando os filtros 
+
+dfmonit_filt <- df_monit %>% 
+  filter(data > "2022-01-01" &
+           !(obs %in% c("Sem geo", "estimado dos dados do ICMBio"))) %>%
+  arrange(data)
+dfmonit_filt
+
+## selecionando a localidade por data para filtrar o numero de transectos
+
+monit <- dfmonit_filt[ ,c("localidade", "data", "n_trans_vis", "n_trans_pres")] %>%
+  group_by(localidade, data) %>%
+  reframe(visuals = max(n_trans_vis),
+          detec = max(n_trans_pres)) %>%
+  arrange(data)
+
+print(monit, n = 44)
+###número maximo de transectos por data e localidade
+
+## obtendo o total de transectos vistos e detecções para cada localidade
+
+monit2 <- monit %>%
+  group_by(localidade) %>%
+  summarise(vis = sum(visuals),
+            det = sum(detec)) %>%
+  arrange(desc(vis))
+
+print(monit2, n = 35)
+
+
+
+# geomorfologia
+
+df_geo = read_delim("data/dados_geo_cs_2024-03-22.csv", 
+                    col_types = list(localidade = col_character(),
+                                     data = col_date(format = "%d/%m/%Y"),
+                                     visibilidade = col_double(),
+                                     faixa_bat = col_character(),
+                                     prof_interface_min = col_double(),
+                                     prof_interface_max = col_double(),
+                                     metodo = col_character(),
+                                     observador = col_character(),
+                                     tempo_geo = col_double(),
+                                     geo_cat = col_character(),
+                                     iar_geo = col_double(),
+                                     n_trans_vis = col_double(),
+                                     geo_id = col_double())
+)
+spec(df_geo)
+df_geo = df_geo[, 1:14]
+df_geo
+
+## agrupando e obtendo o total do IAR para cada geomorfologia em cada localidade
+
+geo <- df_geo[ ,c("localidade", "data", "geo_cat", "iar_geo")] %>%
+  group_by(localidade, data, geo_cat) %>%
+  reframe(iar_geo = mean(iar_geo)) %>%
+  arrange(data)
+
+print(geo, n = 185)
+
+## colocando as geos como coluna (variáveis) e iargeo como linha (valores das variáveis)
+
+geo2 <- geo[ ,c("localidade", "geo_cat", "iar_geo")] %>%
+  group_by(localidade, geo_cat) %>%
+  summarise(iar_geo = mean(sum(iar_geo))) %>%
+  spread(geo_cat, iar_geo)
+
+
+print(geo2, n = 34)
+
+## padronizando as geos
+###média = 0
+###desvio padrão = 1
+
+gc_pad <- as.matrix(geo2$gc - mean(as.matrix(geo2$gc)))/sd(as.matrix(geo2$gc))
+lg_pad <- as.matrix(geo2$lg - mean(as.matrix(geo2$lg)))/sd(as.matrix(geo2$lg))
+mp_pad <- as.matrix(geo2$mp - mean(as.matrix(geo2$mp)))/sd(as.matrix(geo2$mp))
+rpm_pad <- as.matrix(geo2$rpm - mean(as.matrix(geo2$rpm)))/sd(as.matrix(geo2$rpm))
+tf_pad <- as.matrix(geo2$tf - mean(as.matrix(geo2$tf)))/sd(as.matrix(geo2$tf))
+
+geo2$gc_pad <- gc_pad
+geo2$lg_pad <- lg_pad
+geo2$mp_pad <- mp_pad
+geo2$rpm_pad <- rpm_pad
+geo2$tf_pad <- tf_pad
+
+geo_pad <- geo2[, c(1, 7:11)]
+print(geo_pad, n = 34)
+
+
+# unindo os data frames
+
+geomonit <- left_join(monit2, geo_pad)
+
+print(geomonit, n = 182)
+
+
