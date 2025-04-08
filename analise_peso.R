0c0fdf5b3139a6ac52f4ab972304b3471ae4d91a
silveiratcl <silveira.tcl@gmail.com>
2025-04-08 15:34:16 -0300

    update monitoring and geo data

---
+++ analise_peso.R
@@ -0,0 +1,33 @@
+library(readr)
+data <- read_delim("data2.csv",
+                   col_types = list(weight = col_double(),
+                                    class = col_double(),
+                                    n_polyps = col_double()),
+                   delim = ";"
+) 
+
+
+
+
+head(data)
+
+
+model1 <- lm(weight ~ n_polyps, data = data)
+
+summary(model1)
+plot(model1) 
+
+
+model2 <- lm(weight ~ class, data = data)
+
+summary(model2)
+plot(model2) 
+
+
+
+model3 <- glm(weight ~ class, data = data, family = Gamma(link = "inverse"))
+
+summary(model3)
+plot(model3) 
+
+model3
